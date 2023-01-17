56ba3e55a2418747dad8092fbcd34a1df5dddfdd
Trey K <80800860+treykapfer@users.noreply.github.com>
2023-01-16 23:03:18 -0600

    working tabs in projects

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,15 +1,19 @@
 import React from "react";
 
-function Project({ bodyText, title, codeURL, videoURL, demoURL }) {
+function Project({ bodyText, title, codeURL, videoURL, demoURL, active }) {
   return (
     <>
-      <div>{title}</div>
-      <div>{bodyText}</div>
-      <div>
-        <div>{codeURL}</div>
-        <div>{videoURL}</div>
-        <div>{demoURL}</div>
-      </div>
+      {!!active && (
+        <>
+          <div>{title}</div>
+          <div>{bodyText}</div>
+          <div>
+            <div>{codeURL}</div>
+            <div>{videoURL}</div>
+            <div>{demoURL}</div>
+          </div>
+        </>
+      )}
     </>
   );
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import project_data from "./project_data.json";
 import Project from "./Project";
+import Tabs from "../Tabs";
 
 function Projects() {
+  const [projectNames, setProjectNames] = useState();
+  const [active, setActive] = useState();
+
+  useEffect(() => {
+    if (!!project_data?.projects) {
+      let newNames = project_data.projects.map((project) => {
+        return project.title;
+      });
+      setProjectNames(newNames);
+      setActive(newNames[0]);
+    }
+  }, [project_data]);
+
   return (
     <>
-      <div>
-        {project_data.projects.map((project) => (
-          <Project
-            key={project.id}
-            bodyText={project.body_text}
-            title={project.title}
-            codeURL={project.code_url}
-            videoURL={project.video_url}
-            demoURL={project.demo_url}
-          />
-        ))}
-      </div>
-      ;
+      {!!projectNames && (
+        <>
+          <Tabs tabNames={projectNames} active={active} setActive={setActive} />
+          <div>
+            {project_data.projects.map((project) => (
+              <Project
+                key={project.id}
+                active={active === project.title}
+                bodyText={project.body_text}
+                title={project.title}
+                codeURL={project.code_url}
+                videoURL={project.video_url}
+                demoURL={project.demo_url}
+              />
+            ))}
+          </div>
+          ;
+        </>
+      )}
     </>
   );
 }
