13244724cb6980a9ff44250f4883050419665878
Trey K <80800860+treykapfer@users.noreply.github.com>
2023-01-16 23:47:40 -0600

    tab styling

--- src/components/Tabs/Tab.js
+++ src/components/Tabs/Tab.js
@@ -3,13 +3,13 @@ import styles from "./Tabs.module.css";
 
 function Tab({ active, setActive, name, style }) {
   return (
-    <div className={[styles.tabWrapper, style]}onClick={() => setActive(name)}>
+    <button className={styles.tabWrapper}onClick={() => setActive(name)}>
       {!!active ? (
         <div className={styles.activeTab}>{name}</div>
       ) : (
         <div className={styles.inactiveTab}>{name}</div>
       )}
-    </div>
+    </button>
   );
 }
 
