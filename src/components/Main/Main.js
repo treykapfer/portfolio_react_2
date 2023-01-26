cb707deb3077d8e2593b15bcf10c562796e9a678
Trey K <80800860+treykapfer@users.noreply.github.com>
2023-01-25 22:49:26 -0600

    tweaks

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,8 +9,10 @@ function Main() {
     <div className={styles.pageContainer}>
       {/* Here there is options on the right, with a blank tv with static on the left.
       Main page - on the right there will be "About" and "Project" options - Clicking on the about will display the about text on the tv. Clicking on the projects button will expand a dropdown that shows projects. then when you click on a project, the info will display on the tv. At the bttom, there is the footer info */}
-      <About />
-      <Projects />
+      <main>
+        <About />
+        <Projects />
+      </main>
       <Footer />
     </div>
   );
