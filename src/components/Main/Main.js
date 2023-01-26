58cb7454cd1f648e93a53f6b11dc106c7f9a9a31
Trey K <80800860+treykapfer@users.noreply.github.com>
2023-01-25 23:57:40 -0600

    add tv

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import About from "../About";
 import Footer from "../Footer";
 import Projects from "../Projects";
+import TV from "../TV";
 import styles from "./Main.module.css";
 
 function Main() {
@@ -10,8 +11,11 @@ function Main() {
       {/* Here there is options on the right, with a blank tv with static on the left.
       Main page - on the right there will be "About" and "Project" options - Clicking on the about will display the about text on the tv. Clicking on the projects button will expand a dropdown that shows projects. then when you click on a project, the info will display on the tv. At the bttom, there is the footer info */}
       <main>
-        <About />
-        <Projects />
+        <TV />
+        <div className={styles.sideMenu}>
+          <About />
+          <Projects />
+        </div>
       </main>
       <Footer />
     </div>
---
+++ src/components/TV/TV.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./TV.module.css";
+
+function TV() {
+  return (
+    <div className={styles.tv}>
+      <div>TV Text</div>
+    </div>
+  );
+}
+
+export default TV;
